79cac59f61ea52f4de125b0e5237b76106176e96
Maruti Pundlik Dalvi <maruti@Marutis-MacBook-Air.local>
2025-10-22 15:04:50 +0530

    chatt backend done

--- server/server.js
+++ server/server.js
@@ -4,21 +4,58 @@ import cors from 'cors';
 import http from 'http'
 import { connectDB } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
+import messageRouter from './routes/messageRoutes.js';
+import {Server} from 'socket.io'
 
 //create express app and http server
 const app = express();
 const server = http.createServer(app);
 
-//middleware setup
+// initialize socket.io server
+export const io = new Server(server,{
+    cors: {
+        origin: "",
+    }
+}
+);
+
+//store online users
+export const userSocketMap = {};  //{userId: socketId}
+
+//socket.io connection handler 
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId)
+
+    if(userId) userSocketMap[userId] = socket.id;
+
+    //emit online users to all connected clients
+    io.emit("onlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", userId)
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
+
+});
+
+
+// Middleware setup
 app.use(express.json({limit: '4mb'}));
 app.use(cors());
 
+
+// Routes setup
 app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
 // app.use("/api/chat", chatRouter);
 
+// Connect to MongoDB
 await connectDB();
-//start server
+
+// Start server
 const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
